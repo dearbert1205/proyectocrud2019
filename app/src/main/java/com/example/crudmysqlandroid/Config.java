c56dbeaeeb215e4af5a74186decf7756c0c82c2b
dearbert1205 <51801695+dearbert1205@users.noreply.github.com>
2019-11-08 22:16:03 +0100

    ultimo commit

--- app/src/main/java/com/example/crudmysqlandroid/Config.java
+++ app/src/main/java/com/example/crudmysqlandroid/Config.java
@@ -3,7 +3,7 @@ package com.example.crudmysqlandroid;
 public class Config {
 
     //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
-    ///
+/*
     public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
     public static final String urlActualizar = "http://mjgl.com.sv/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
@@ -14,9 +14,9 @@ public class Config {
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-    */
+*/
 
-    /*
+
     ////URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.*/
 
 
