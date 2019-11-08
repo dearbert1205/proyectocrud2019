1a5818ea02bec032331f13fa22800c58656d4cdc
dearbert1205 <51801695+dearbert1205@users.noreply.github.com>
2019-11-08 22:19:57 +0100

    commit 12

--- app/src/main/java/com/example/crudmysqlandroid/Config.java
+++ app/src/main/java/com/example/crudmysqlandroid/Config.java
@@ -3,7 +3,7 @@ package com.example.crudmysqlandroid;
 public class Config {
 
     //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
-/*
+
     public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
     public static final String urlActualizar = "http://mjgl.com.sv/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
@@ -14,13 +14,13 @@ public class Config {
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-*/
+
 
 
     ////URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.*/
 
 
-    public static final String urlGuardar = "http://192.168.1.235/mysqlcrud/guardar.php";                               //Guardar datos DB.
+    /*public static final String urlGuardar = "http://192.168.1.235/mysqlcrud/guardar.php";                               //Guardar datos DB.
 
     public static final String urlEliminar = "http://192.168.1.235/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
     public static final String urlActualizar = "http://192.168.1.235/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
@@ -31,6 +31,6 @@ public class Config {
     public static final String urlConsultaCodigo = "http://192.168.1.235/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://192.168.1.235/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
     public static final String urlConsultaAllArticulos = "http://192.168.1.235/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-
+    */
 
 }
